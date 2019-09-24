3ebe065dcde5ea2ce2cb6523ad0f261e89b6a8fc
SaneeyaIqbal <s.saneeya.iqbal@gmail.com>
2019-09-24 22:34:22 +0530

    tictactoe game

--- game.py
+++ game.py
@@ -1,90 +1,83 @@
-#n = input()
-end=False
-board=[1,2,3,4,5,6,7,8,9]
-win=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+def tic_tac_toe():
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end = False
+    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 
-class tictactoe:
-
-    def Game(self):
-    #board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    #win=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-
-    def Board():
-        print(board[0],board[1],board[2])
-        print(board[3],board[4],board[5])
-        print(board[6],board[7],board[8])
+    def draw():
+        print(board[0], board[1], board[2])
+        print(board[3], board[4], board[5])
+        print(board[6], board[7], board[8])
         print()
 
-def player1():
-    n = position()
-    if board[n] == 'X' or board[n] == 'O':
-        print("do not go there")
-        player1()
-    else:
-        board[n] = 'X'
+    def p1():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p1()
+        else:
+            board[n] = "X"
 
-def player2():
-    if board[n] == 'X' or board[n] == 'O':
-        print("do not go there")
-        player2()
-    else:
-        board[n] = 'O'
+    def p2():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p2()
+        else:
+            board[n] = "O"
 
+    def choose_number():
+        while True:
+            while True:
+                a = input()
+                try:
+                    a  = int(a)
+                    a -= 1
+                    if a in range(0, 9):
+                        return a
+                    else:
+                        print("\nThat's not on the board. Try again")
+                        continue
+                except ValueError:
+                   print("\nThat's not a number. Try again")
+                   continue
 
-def position():
-    while True:
+    def check_board():
+        count = 0
+        for a in win_commbinations:
+            if board[a[0]] == board[a[1]] == board[a[2]] == "X":
+                print("Player 1 Wins!\n")
+                print("Congratulations!\n")
+                return True
 
-        a = input()
-        try:
-            a = int(a)
-            a -= 1
-            if a in range(0,9):
-                return a
-            else:
-                print("there is no board")
-            print("there is no number")
-
-def board_entry_win():
-    count = 0
-    for a in win:
-        if board[a[0]] == board[a[1]] == board[a[2]] == 'X':
-            print("player1 wins")
-            return
-
-        if board[a[0]] == board[a[1]] == board[a[2]] == 'O':
-            print("player 2 wins")
-            return
-
+            if board[a[0]] == board[a[1]] == board[a[2]] == "O":
+                print("Player 2 Wins!\n")
+                print("Congratulations!\n")
+                return True
         for a in range(9):
-            if board[a]=='X' or board[a]=='O':
-                count+=1
-
-            if count==9:
-                print("all spaces are occupied and the game ends")
+            if board[a] == "X" or board[a] == "O":
+                count += 1
+            if count == 9:
+                print("The game ends in a Tie\n")
                 return True
 
     while not end:
-        end = board_entry_win()
+        draw()
+        end = check_board()
         if end == True:
             break
-        print("player 1 chose to play")
-        player1()
+        print("Player 1 choose where to place a cross")
+        p1()
         print()
-        end = board_entry_win()
+        draw()
+        end = check_board()
         if end == True:
             break
-        print("player 2 chose to play")
-        player2()
+        print("Player 2 choose where to place a nought")
+        p2()
         print()
 
+    if input("Play again (y/n)\n") == "y":
+        print()
+        tic_tac_toe()
 
-board_entry_win()
-
-
-#def win_situation():
- #   if win_situation()
-
-#def input():
-
-
-Board()
+tic_tac_toe()
